a5bc05ef1b941dd945f88673d84429855d5f0cd5
Danilo Humberto <danilohumberto12@hotmail.com>
2025-05-13 15:53:24 -0300

    feat: add axios and react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./hooks/theme-provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryCliente = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryCliente}>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
