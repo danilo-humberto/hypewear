a8144c8c0ca2aea697365d4a9e1784d063898f24
Danilo <danilohumberto12@hotmail.com>
2025-05-15 14:44:31 -0300

    chore: changing text

--- src/components/header/discount.tsx
+++ src/components/header/discount.tsx
@@ -2,8 +2,7 @@ const Discount = () => {
   return (
     <section className="bg-foreground flex items-center justify-center w-full h-10 p-5">
       <p className="text-background/75 text-[10px] text-center sm:text-[12px]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sed quos
-        molestias explicabo facilis.
+        Enjoy Discount $50 off, voucher code "SUMMER50". Valid until May 31st
       </p>
     </section>
   );
