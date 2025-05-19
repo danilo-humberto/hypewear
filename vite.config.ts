c3a41fe7574b7d2bbfe4f53cd65f717dc0366f8a
Danilo <danilohumberto12@hotmail.com>
2025-05-19 20:48:55 -0300

    fix: taking github actions of the project

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hypewear/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
