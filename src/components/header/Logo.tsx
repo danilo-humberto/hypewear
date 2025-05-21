77132d9cb35c9f73b3d4d8da272a0fa4035bbb6a
Danilo <danilohumberto12@hotmail.com>
2025-05-21 17:16:44 -0300

    fix: trying loading pictures

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -2,10 +2,15 @@ import { useTheme } from "@/hooks/theme-provider";
 
 const Logo = () => {
   const { theme } = useTheme();
+  const baseUrl = process.env.PUBLIC_URL || "";
   return (
     <div className="flex items-center absolute z-50 left-2 sm:left-5 xl:left-10">
       <img
-        src={theme === "light" ? "logo-light.png" : "logo-dark.png"}
+        src={
+          theme === "light"
+            ? `${baseUrl}logo-light.png`
+            : `${baseUrl}logo-dark.png`
+        }
         alt="logo da HypeWear"
         className="w-12 h-11"
       />
