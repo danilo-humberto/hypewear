8333a7bca15e03b7936ec6e7d7243856ec755a85
Danilo Humberto <danilohumberto12@hotmail.com>
2025-10-21 14:35:44 -0300

    fix: atualizando para inserir o layout para as outras paginas

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import DetailsProduct from "./pages/DetailsProduct";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path="/product/:id" element={<DetailsProduct />} />
+        </Route>
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
 
