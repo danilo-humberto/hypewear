4ec44742b7ab872b7b649d3537d96275f451c149
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-10-29 20:24:23 -0300

    resolve axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-api.interceptors.request.use(
-  (config) => {
-    // Pega o token do localStorage (ou de onde você o salvar após o login)
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      // Se o token existir, adiciona no cabeçalho de autorização
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
-export default api;
-=======
 import axios from "axios";
 
 const api = axios.create({
@@ -35,4 +8,3 @@ const api = axios.create({
 });
 
 export default api;
->>>>>>> 42a1f3cbd5db1f33419d4359669e3b3889a17931
