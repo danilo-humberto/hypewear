ca90c91d6b16886e5f5e3c0c54cbcc426ba6e368
Danilo Humberto <danilohumberto12@hotmail.com>
2025-10-29 13:52:52 -0300

    fix: atualizando para passar o id do produto por params

--- src/components/wears/ProductCards.tsx
+++ src/components/wears/ProductCards.tsx
@@ -1,65 +1,47 @@
-import { useCart } from "@/hooks/useCart";
-import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
-import StarRating from "./StarRating";
+import { Link } from "react-router-dom";
 
-interface Rating {
-  rate: number;
-  count: number;
-}
 interface CardsProps {
   id: number;
-  title: string;
+  name: string;
   description: string;
   price: number;
-  image: string;
-  category: string;
-  rating: Rating;
+  imagem: string;
+  category: {
+    name: string;
+  };
 }
 
-const Cards = ({
-  title,
-  description,
-  price,
-  image,
-  category,
-  rating,
-  id,
-}: CardsProps) => {
-  const { addToCart: AddtoCart } = useCart();
+const Cards = ({ name, description, price, imagem, id }: CardsProps) => {
   return (
     <Card className="shadow-none">
       <CardContent className="w-full px-2">
         <div className="w-full h-[300px]">
           <img
-            src={image}
-            alt={title}
+            src={imagem}
+            alt={name}
             className="rounded-md object-contain w-full h-full"
           />
         </div>
         <div className="p-2 flex justify-between gap-4">
           <div className="overflow-hidden flex-auto">
-            <p className="truncate">{title}</p>
-            <p className="truncate text-muted-foreground">{description}</p>
-            <p className="truncate text-muted-foreground">
-              Category: {category}
-            </p>
+            <p className="truncate">{name}</p>
+            <p className="truncate text-muted-foreground pt-2">{description}</p>
           </div>
           <span className="font-bold">
             {new Intl.NumberFormat("en-US", {
               style: "currency",
-              currency: "USD",
+              currency: "BRL",
             }).format(price)}
           </span>
         </div>
-        <div className="p-2 flex justify-between items-center">
-          <Button variant={"outline"} onClick={() => AddtoCart(id)}>
-            Add to Cart
-          </Button>
-          <div className="flex gap-1 items-center">
-            <span>({rating.count})</span>
-            <StarRating rating={rating.rate} />
-          </div>
+        <div className="px-2 flex justify-between items-center">
+          <Link
+            to={`/product/${id}`}
+            className="w-full bg-accent p-2 text-center rounded-sm text-foreground hover:bg-accent/80 transition-all duration-300"
+          >
+            Saber mais
+          </Link>
         </div>
       </CardContent>
     </Card>
