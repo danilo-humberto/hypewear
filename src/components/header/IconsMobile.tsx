78913225022219450c9e7ae7ccdd68926820c36f
Danilo Humberto <danilohumberto12@hotmail.com>
2025-10-30 10:44:43 -0300

    fix: tirando o icone de pesquisa

--- src/components/header/IconsMobile.tsx
+++ src/components/header/IconsMobile.tsx
@@ -1,5 +1,4 @@
 import { ModeToggle } from "../mode-toggle";
-import SearchMobile from "./SearchMobile";
 import MenuSheet from "./MenuSheet";
 import Cart from "./Cart";
 
@@ -7,7 +6,6 @@ const IconsMobile = () => {
   return (
     <div className="flex gap-3 items-center absolute right-5 lg:hidden">
       <ModeToggle />
-      <SearchMobile />
       <Cart />
       <MenuSheet />
     </div>
