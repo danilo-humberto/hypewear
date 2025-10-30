583af0d9e6cb194f47a275a57cd1a26d71d369cf
Danilo Humberto <danilohumberto12@hotmail.com>
2025-10-30 14:47:04 -0300

    fix: ajustando o nome da interface, de User para Client

---
+++ src/types/Profile.ts
@@ -0,0 +1,19 @@
+export interface Address {
+  id: string;
+  logradouro: string;
+  numero: string;
+  cidade: string;
+  estado: string;
+  cep: string;
+  bairro: string;
+  complemento: string;
+  isDefault?: boolean;
+}
+
+export interface Client {
+  id: string;
+  name: string;
+  email: string;
+  numberPhone: string;
+  createdAt: string;
+}
