1111cd00ae2912fc05c95efb2deb3bdd20c43721
Danilo Humberto <danilohumberto12@hotmail.com>
2025-10-30 10:45:50 -0300

    feat: adicionando feedback visual na requisicao

--- src/hooks/queries/useClient.ts
+++ src/hooks/queries/useClient.ts
@@ -1,5 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getClient, updateClient } from "@/api/clients.endpoint";
+import { toast } from "sonner";
 
 export const useClient = (id: string) =>
   useQuery({
@@ -13,10 +14,12 @@ export const useUpdateClient = (id: string) => {
   return useMutation({
     mutationFn: (payload: any) => updateClient(id, payload),
     onSuccess: (data) => {
+      toast.success("Dados atualizados com sucesso!");
       queryClient.setQueryData(["clients", id], (old: any) => ({
         ...old,
         ...data,
       }));
     },
+    onError: () => toast.error("Erro ao atualizar dados!"),
   });
 };
