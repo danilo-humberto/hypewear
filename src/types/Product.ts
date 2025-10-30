256c1eea5397c3bba919c96fe753e56139c96968
Danilo Humberto <danilohumberto12@hotmail.com>
2025-10-30 16:12:40 -0300

    feat: adicionando o type de Produto

---
+++ src/types/Product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  estoque: number;
+  imagem: string;
+  status: string;
+  categoryId: string;
+}
