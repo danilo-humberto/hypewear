d317017f9767bb37f09d26009d2b336c12979516
Danilo Humberto <danilohumberto12@hotmail.com>
2025-11-27 15:58:32 -0300

    feat: adicionando requests que faltam

--- src/api/clients.endpoint.ts
+++ src/api/clients.endpoint.ts
@@ -1,5 +1,10 @@
 import api from "./axios";
 
+export const getClients = async () => {
+  const { data } = await api.get("/clients");
+  return data;
+};
+
 export const getClient = async (id: string) => {
   const { data } = await api.get(`/clients/${id}`);
   return data;
@@ -7,5 +12,10 @@ export const getClient = async (id: string) => {
 
 export const updateClient = async (id: string, payload: unknown) => {
   const { data } = await api.patch(`/clients/${id}`, payload);
+  return data;
+};
+
+export const deleteClient = async (id: string) => {
+  const { data } = await api.delete(`/clients/${id}`);
   return data;
 };
